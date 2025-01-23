11c4a8b770d18b632eace8129a6856d8e557f631
Galven <122519717+Inquisitivrx@users.noreply.github.com>
2025-01-23 17:36:27 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Luna.java
@@ -0,0 +1,13 @@
+public class Luna {
+    public static void main(String[] args) {
+        // Print a greeting message
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm [YOUR CHATBOT NAME]");
+        System.out.println(" What can I do for you?");
+
+        // Print a farewell message
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
