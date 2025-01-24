7f842ada2c596a82e620767e155529473c837d1a
Galven <e0920634@u.nus.edu>
2025-01-25 03:00:21 +0800

    Echo

--- src/main/java/Luna.java
+++ src/main/java/Luna.java
@@ -1,13 +1,34 @@
+import java.util.Scanner;
+
 public class Luna {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         // Print a greeting message
         System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm [YOUR CHATBOT NAME]");
+        System.out.println(" Hello! I'm Luna");
         System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
 
-        // Print a farewell message
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        while (true) {
+            // Read user input
+            String userInput = scanner.nextLine();
+
+            if (userInput.equalsIgnoreCase("bye")) {
+                // Exit if the user types "bye"
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            } else {
+                // Echo the command entered by the user
+                System.out.println("____________________________________________________________");
+                System.out.println(" " + userInput);
+                System.out.println("____________________________________________________________");
+            }
+        }
+
+        // Close the scanner
+        scanner.close();
     }
 }
