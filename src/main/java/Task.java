4178bb3c9670bf690e634c59381555422408d523
Galven <e0920634@u.nus.edu>
2025-02-01 16:29:15 +0800

    implement parsing of dates and times for deadline

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -33,25 +33,30 @@ abstract class Task {
         boolean isDone = parts[1].equals("1");
         String description = parts[2];
 
-        switch (type) {
-            case "T":
-                Todo todo = new Todo(description);
-                if (isDone) todo.markAsDone();
-                return todo;
-            case "D":
-                if (parts.length < 4) throw new IOException("Corrupted deadline data");
-                Deadline deadline = new Deadline(description, parts[3]);
-                if (isDone) deadline.markAsDone();
-                return deadline;
-            case "E":
-                if (parts.length < 5) throw new IOException("Corrupted event data");
-                Event event = new Event(description, parts[3], parts[4]);
-                if (isDone) event.markAsDone();
-                return event;
-            default:
-                throw new IOException("Unknown task type: " + type);
+        try {
+            switch (type) {
+                case "T":
+                    Todo todo = new Todo(description);
+                    if (isDone) todo.markAsDone();
+                    return todo;
+                case "D":
+                    if (parts.length < 4) throw new IOException("Corrupted deadline data");
+                    Deadline deadline = new Deadline(description, parts[3]);
+                    if (isDone) deadline.markAsDone();
+                    return deadline;
+                case "E":
+                    if (parts.length < 5) throw new IOException("Corrupted event data");
+                    Event event = new Event(description, parts[3], parts[4]);
+                    if (isDone) event.markAsDone();
+                    return event;
+                default:
+                    throw new IOException("Unknown task type: " + type);
+            }
+        } catch (IllegalArgumentException e) {
+            throw new IOException("Invalid date format in stored data: " + e.getMessage());
         }
     }
+
 
     @Override
     public String toString() {
