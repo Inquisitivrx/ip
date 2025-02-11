91d01101514cde67ce45ccaaecc3268b655e7df4
Galven <122519717+Inquisitivrx@users.noreply.github.com>
2025-02-11 12:24:51 +0800

    Update JavaDoc comments

--- src/main/java/luna/task/TaskList.java
+++ src/main/java/luna/task/TaskList.java
@@ -51,6 +51,13 @@ public class TaskList {
         return tasks.remove(index);
     }
 
+    /**
+     * Retrieves a task from the list based on index.
+     *
+     * @param index The index of the task to retrieve.
+     * @return The requested task.
+     * @throws LunaException If the index is out of bounds.
+     */
     public Task getTask(int index) throws LunaException {
         if (index < 0 || index >= tasks.size()) {
             throw new LunaException("luna.task.Task index out of range.");
@@ -58,14 +65,31 @@ public class TaskList {
         return tasks.get(index);
     }
 
+    /**
+     * Marks a task as done based on its index.
+     *
+     * @param index The index of the task to mark as done.
+     * @throws LunaException If the index is out of bounds.
+     */
     public void markTaskDone(int index) throws LunaException {
         getTask(index).markAsDone();
     }
 
+    /**
+     * Marks a task as not done based on its index.
+     *
+     * @param index The index of the task to mark as not done.
+     * @throws LunaException If the index is out of bounds.
+     */
     public void markTaskNotDone(int index) throws LunaException {
         getTask(index).markAsNotDone();
     }
 
+    /**
+     * Lists all tasks in the task list.
+     *
+     * @param ui The UI instance used to display the tasks.
+     */
     public void listTasks(Ui ui) {
         ui.showMessage("Here are the tasks in your list:");
         for (int i = 0; i < tasks.size(); i++) {
@@ -74,6 +98,11 @@ public class TaskList {
         ui.showBorder();
     }
 
+    /**
+     * Retrieves the list of tasks.
+     *
+     * @return The list of tasks.
+     */
     public List<Task> getTasks() {
         return tasks;
     }
